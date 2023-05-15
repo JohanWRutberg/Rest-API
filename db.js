1309f405a90b4ce23f2bab0945f5d8a8de0873ac
JoRuPeRu <johan.rutberg@iths.se>
2023-05-15 11:11:39 +0200

    db update

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'students',
-    password: 'Pappa72&Mamma84',
+    password: 'testtest',
     port: 5432
-})
+});
 
-module.exports = pool
+module.exports = pool;
